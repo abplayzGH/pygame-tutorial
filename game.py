2c2791bf2b40c024897791d4033d3741c85b09ff
--global <adam@impostertwo.com>
2024-04-15 12:53:46 -0500

    Snail

--- game.py
+++ game.py
@@ -6,9 +6,16 @@ pygame.init()
 screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption("Runner")
 clock = pygame.time.Clock()
+test_font = pygame.font.Font("font/Pixeltype.ttf", 50)
 
-test_surface = pygame.Surface((100,200))
-test_surface.fill("Red")
+
+sky_surface = pygame.image.load("graphics/Sky.png").convert_alpha()
+ground_surface = pygame.image.load("graphics/ground.png").convert_alpha()
+text_surface = test_font.render("Hello World", False, "Black").convert_alpha()
+
+snail_surface = pygame.image.load("graphics/snail/snail1.png").convert_alpha()
+snail_x = 600
+
 
 while True:
     for event in pygame.event.get():
@@ -16,7 +23,15 @@ while True:
             pygame.quit()
             exit()
              
-    screen.blit(test_surface, (0,0) )
+    screen.blit(sky_surface, (0,0) )
+    screen.blit(ground_surface, (0,300))
+    screen.blit(text_surface, (300,50))
+    snail_x -= 2
+    
+    if snail_x < -100:
+        snail_x = 800
+    
+    screen.blit(snail_surface,(snail_x,250))
     
     pygame.display.update()
     clock.tick(60)
